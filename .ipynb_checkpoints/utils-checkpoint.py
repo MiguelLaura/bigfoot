8416dfb4559771fba9d9f039d837c70181fa24cc
Laura Miguel <miguellaura240@gmail.com>
2023-04-27 13:43:21 +0200

    ajout de CHA et kmeans

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -3,36 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
 from sklearn.manifold import MDS
 from scipy.spatial.distance import cdist
-
-
-def plot_Shepard(mds_model, plot=True):
-    """Affiche le diagramme de Shepard et retourne un couple contenant les
-    dissimilarités originales et les distances apprises par le
-    modèle.
-    """
-
-    assert isinstance(mds_model, MDS)
-
-    # Inter-distances apprises
-    dist = cdist(mds_model.embedding_, mds_model.embedding_)
-    idxs = np.tril_indices_from(dist, k=-1)
-    dist_mds = dist[idxs]
-
-    # Inter-distances d'origine
-    dist = mds_model.dissimilarity_matrix_
-    dist_orig = dist[idxs]
-
-    dists = np.column_stack((dist_orig, dist_mds))
-
-    if plot:
-        f, ax = plt.subplots()
-        range = [dists.min(), dists.max()]
-        ax.plot(range, range, 'r--')
-        ax.scatter(*dists.T)
-        ax.set_xlabel('Dissimilarités')
-        ax.set_ylabel('Distances')
-
-    return (*dists.T,)
+import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
+from scipy.stats import norm
+from scipy import linalg
+from sklearn.decomposition import PCA
 
 
 # Taken from https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html
@@ -61,70 +37,83 @@ def plot_dendrogram(model, **kwargs):
     dendrogram(linkage_matrix, **default_kwargs)
 
 
+def scatterplot_pca(
+    columns=None, hue=None, style=None, data=None, pc1=1, pc2=2, **kwargs
+):
+    """Diagramme de dispersion dans le premier plan principal.
 
+    Permet d'afficher un diagramme de dispersion lorsque les données
+    ont plus de deux dimensions. L'argument `columns` spécifie la
+    liste des colonnes à utiliser pour la PCA dans le jeu de données
+    `data`. Les arguments `style` et `hue` permettent de spécifier la
+    forme et la couleur des marqueurs. Les arguments `pc1` et `pc2`
+    permettent de sélectionner les composantes principales (par défaut
+    la première et deuxième). Retourne l'objet `Axes` ainsi que le
+    modèle `PCA` utilisé pour réduire la dimension.
 
-# def plot_clustering(data, labels, markers=None, ax=None, **kwargs):
-#     """Affiche dans leur premier plan principal les données `data`,
-# colorée par `labels` avec éventuellement des symboles `markers`.
-#     """
+    :param columns: Les colonnes quantitatives de `data` à utiliser
+    :param hue: La colonne de coloration
+    :param style: La colonne du style
+    :param data: Le dataFrame Pandas
+    :param pc1: La composante en abscisse
+    :param pc2: La composante en ordonnée
 
-#     if ax is None:
-#         ax = plt.gca()
+    """
+     # Select relevant columns (should be numeric)
+    data_quant = data if columns is None else data[columns]
+    data_quant = data_quant.drop(
+        columns=[e for e in [hue, style] if e is not None], errors="ignore"
+    )
 
-#     # Reduce to two dimensions
-#     if data.shape[1] == 2:
-#         data_pca = data.to_numpy()
-#     else:
-#         pca = PCA(n_components=2)
-#         data_pca = pca.fit_transform(data)
+    # Reduce to two dimensions if needed
+    if data_quant.shape[1] == 2:
+        data_pca = data_quant
+        pca = None
+    else:
+        n_components = max(pc1, pc2)
+        pca = PCA(n_components=n_components)
+        data_pca = pca.fit_transform(data_quant)
+        data_pca = pd.DataFrame(
+            data_pca[:, [pc1 - 1, pc2 - 1]], columns=[f"PC{pc1}", f"PC{pc2}"]
+        )
 
-#     COLORS = np.array(['blue', 'green', 'red', 'purple', 'gray', 'cyan'])
-#     _, labels = np.unique(labels, return_inverse=True)
-#     colors = COLORS[labels]
+    # Keep name, force categorical data for hue and steal index to
+    # avoid unwanted alignment
+    if isinstance(hue, pd.Series):
+        if not hue.name:
+            hue.name = "hue"
+        hue_name = hue.name
+    elif isinstance(hue, str):
+        hue_name = hue
+        hue = data[hue]
+    elif isinstance(hue, np.ndarray):
+        hue = pd.Series(hue, name="class")
+        hue_name = "class"
 
-#     if markers is None:
-#         ax.scatter(*data_pca.T, c=colors)
-#     else:
-#         MARKERS = "o^sP*+xD"
+    hue = hue.astype("category")
+    hue.index = data_pca.index
+    hue.name = hue_name
 
-#         # Use integers
-#         markers_uniq, markers = np.unique(markers, return_inverse=True)
+    if isinstance(style, pd.Series):
+        if not style.name:
+            style.name = "style"
+        style_name = style.name
+    elif isinstance(style, str):
+        style_name = style
+        style = data[style]
+    elif isinstance(style, np.ndarray):
+        style = pd.Series(style, name="style")
+        style_name = "style"
 
-#         for marker in range(len(markers_uniq)):
-#             data_pca_marker = data_pca[markers == marker, :]
-#             colors_marker = colors[markers == marker]
-#             ax.scatter(*data_pca_marker.T, c=colors_marker, marker=MARKERS[marker])
+    full_data = data_pca
+    if hue is not None:
+        full_data = pd.concat((full_data, hue), axis=1)
+        kwargs["hue"] = hue_name
+    if style is not None:
+        full_data = pd.concat((full_data, style), axis=1)
+        kwargs["style"] = style_name
 
-#     if 'centers' in kwargs and 'covars' in kwargs:
-#         if data.shape[1] == 2:
-#             centers_2D = kwargs['centers']
-#             covars_2D = kwargs['covars']
-#         else:
-#             centers_2D = pca.transform(kwargs["centers"])
-#             covars_2D = [
-#                 pca.components_ @ c @ pca.components_.T
-#                 for c in kwargs['covars']
-#             ]
+    x, y = data_pca.columns
+    ax = sns.scatterplot(x=x, y=y, data=full_data, **kwargs)
 
-#         p = 0.9
-#         sig = norm.ppf(p**(1/2))
-
-#         for i, (covar_2D, center_2D) in enumerate(zip(covars_2D, centers_2D)):
-#             v, w = linalg.eigh(covar_2D)
-#             print(v)
-#             v = 2. * sig * np.sqrt(v)
-
-#             u = w[0] / linalg.norm(w[0])
-#             if u[0] == 0:
-#                 angle = np.pi / 2
-#             else:
-#                 angle = np.arctan(u[1] / u[0])
-
-#             color = COLORS[i]
-#             angle = 180. * angle / np.pi  # convert to degrees
-#             ell = mpl.patches.Ellipse(center_2D, v[0], v[1], 180. + angle, color=color)
-#             ell.set_clip_box(ax.bbox)
-#             ell.set_alpha(0.5)
-#             ax.add_artist(ell)
-
-#     return ax
+    return ax, pca
